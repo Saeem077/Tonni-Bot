1286bb831c0cd26c194b95141caa2227b2a71565
Saeem Sheikh (Editor) <saeemsheikh077@gmail.com>
2025-06-15 17:22:42 +0600

    Update 0out.js

--- Priyansh/commands/0out.js
+++ Priyansh/commands/0out.js
@@ -1,8 +1,8 @@
 module.exports.config = {
     name: "out",
     version: "1.0.0",
-    hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    hasPermssion: 1,
+    credits: "𝙎𝘼𝙀𝙀𝙈 𝙎𝙃𝙀𝙄𝙆𝙃",
     description: "",
     commandCategory: "Admin",
     usages: "out [id]",
